f4db78757be4e6bc3c03091757b30ab6acaf7ba2
Janardhan B <147693850+janardhan345@users.noreply.github.com>
2026-01-29 22:50:30 +0530

    Database connection, express app created.

---
+++ Backend/src/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect
+        (`${process.env.MONGODB_URI}`)
+        console.log(`\n Database Connected! 
+            ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log(`Database Connection Failed!`, error);
+        process.exit(1);
+    }
+}
+export default connectDB;
