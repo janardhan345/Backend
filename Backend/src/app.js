a937f30b5c1634baa69431c21f611e7ccdd2ab06
Janardhan B <147693850+janardhan345@users.noreply.github.com>
2026-02-05 14:29:42 +0530

    loginUser API created and tested successfully.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,14 @@
 import express from "express"
 
 const app = express();
+app.use(express.json());
 
 // importing routes
 import userRouter from "./routes/user.route.js"
 
 //routes declaration
+app.use("/api/v1/users",userRouter);
 
+//example route https://localhost:4000/api/v1/users/register
 
 export default app; 
--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -11,7 +11,7 @@ const registerUser = async(req, res) => {
             // If already signed up 
             const existing = await User.findOne({ email:email.toLowerCase()})
             if(existing){
-                return res.status(400).json({Message: "User Already exists"})
+                return res.status(200).json({Message: "User Already exists"})
             }
             const user = await User.create({
                 username,
@@ -20,12 +20,45 @@ const registerUser = async(req, res) => {
                 loggedIn: false
             });
             
-            res.status(400).json({Message:"user registered",
+            res.status(200).json({Message:"user registered",
                 user: { id:user.id, email: user.email, username: user.username}})
     } catch (error) {
         res.status(500).json({Message: "Internal Server Error", error:error.message});
     }
 }
+
+const  loginUser = async(res, req) => {
+    try {
+         const {email, password} = req.body;
+
+         const user = await User.findOne({
+            email:email.toLowerCase()
+         });
+
+         if(!user) return res.status(400).json({
+            message:"User doesn't Exist"
+         })
+
+          const isMatch =  await user.comparePassword(password);
+          if(!isMatch)
+            return res.status(400).json({
+                message:"Invalid password"
+            })
+            
+            res.status(200).json({message:"User loggedin",
+                user:{
+                    id:user._id,
+                    email:user.email,
+                    username:user.username
+                }
+            })
+
+    } catch (error) {
+        res.status(500).json({
+            message:"Internal server Error"
+        })
+    }
+}
 export {
-    registerUser
+    registerUser, loginUser
 };
