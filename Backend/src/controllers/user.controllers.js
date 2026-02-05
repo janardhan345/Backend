7ce0ed5f7ecfce45a0dff7250dd4e326b349c5dc
Janardhan B <147693850+janardhan345@users.noreply.github.com>
2026-02-05 14:41:55 +0530

    auth API : final api is logout API , which is tested.

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -59,6 +59,30 @@ const  loginUser = async(req, res) => {
         })
     }
 }
+
+const logoutUser = async(req, res) =>{
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({
+            email
+        });
+
+        if(!user) return res.status(404).json({ 
+            message: "User not found"
+        });
+
+        return res.status(200).json({
+            message: "Successfully logged out"
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server error",
+            error:error
+        });
+    }
+}
 export {
-    registerUser, loginUser
+    registerUser, loginUser, logoutUser
 };
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { loginUser,registerUser } from "../controllers/user.controllers.js";
+import { loginUser,logoutUser,registerUser } from "../controllers/user.controllers.js";
 
 const router = Router();
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').post(logoutUser);
 export default router;
